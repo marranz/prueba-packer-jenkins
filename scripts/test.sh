f512e797cd020f4f17da56d2aa61f9c6318cebc7
Your Name <you@example.com>
2017-08-22 16:28:18 +0100

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-aws_access_key=${aws_access_key} aws_secret_key=${aws_secret_key} packer validate ../packet/ubuntu-base.json
-aws_access_key=${aws_access_key} aws_secret_key=${aws_secret_key} packer build ../packet/ubuntu-base.json
+aws_access_key=${aws_access_key} aws_secret_key=${aws_secret_key} packer validate ../packer/ubuntu-base.json
+aws_access_key=${aws_access_key} aws_secret_key=${aws_secret_key} packer build ../packer/ubuntu-base.json
 
